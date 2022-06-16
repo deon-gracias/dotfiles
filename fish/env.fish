4bbb810c4559673b0430334c61b7ff7462e953bf
deon-gracias <deongracias1@gmail.com>
2022-06-16 19:42:06 +0530

    Added GOPATH

--- fish/env.fish
+++ fish/env.fish
@@ -1,8 +1,9 @@
 set -Ux flutter $HOME/.local/share/flutter/bin
 set -Ux EDITOR nvim
 set -Ux CHROME_EXECUTABLE /usr/bin/chromium
-set -Ux ANDROID_SDK_ROOT ~/Android/Sdk
+set -Ux ANDROID_SDK_ROOT $HOME/Android/Sdk
 set -Ux SUDO_EDITOR /usr/bin/nvim
+set -Ux GO_PATH $HOME/Coding/go
 set PATH flutter $HOME/.local/share/flutter/bin $PATH
 set PATH emacs $HOME/.emacs.d/bin $PATH
 set PATH cargo $HOME/.local/cargo/bin $PATH
