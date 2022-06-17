896812d9a9da4a45536f054227071bb2873f2454
deon-gracias <deongracias1@gmail.com>
2022-06-17 13:48:49 +0530

    Moved aliases loading to interactive shell

--- fish/config.fish
+++ fish/config.fish
@@ -6,10 +6,10 @@ set -gx EDITOR nvim                     # Set default editor as neovim
 # Login Shell
 if status is-login
     . ~/.config/fish/env.fish           # Environment variables
-    . ~/.config/fish/aliases.fish       # Aliases
     emacs --daemon                      # Launch emacs server
 end
 
 # Interactive Shell
 if status is-interactive
+    . ~/.config/fish/aliases.fish       # Aliases
 end
