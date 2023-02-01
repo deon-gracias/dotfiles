71053c89bf1579f7f9036ed63a3372ebf4464079
deon-gracias <deongracias1@gmail.com>
2023-02-02 00:34:28 +0530

    Added pocketbase

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ cp -r ncmpcpp ~/.config/ncmpcpp
 cp -r qutebrowser ~/.config/qutebrowser
 
 # Install oh my fish
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 
 # Install vim plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
